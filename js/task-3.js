e7d68a5e1853075456ba1b495bd845a1c9b323b5
Alexsander Rjevskiy <porezkakir2@gmail.com>
2024-11-12 01:14:54 +0300

    1

---
+++ js/task-3.js
@@ -0,0 +1,14 @@
+// function getElementWidth( content , padding , borderNpx ) {
+// content = Number.parseFloat(content);
+// padding = Number.parseFloat(padding);
+//     borderNpx = Number.parseFloat(borderNpx);
+    
+//     return `${content + (padding * 2) + (borderNpx * 2)}`
+
+// };
+
+
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
